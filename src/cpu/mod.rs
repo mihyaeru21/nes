8221131933e018aeba17b4310c488017576d4b9a
Mihyaeru <mihyaeru21@gmail.com>
2020-05-26 02:32:03 +0900

    :sparkles: implement SEI

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,3 +1,4 @@
+use instruction::{Instruction, Kind};
 use register::Registers;
 use std::{cell::RefCell, rc::Rc};
 
@@ -8,11 +9,11 @@ mod register;
 pub struct Cpu {
     registers: Registers,
     rom: Rc<Vec<u8>>, // TODO: RcのやつはBusに切り出す？
-    ram: Rc<RefCell<Vec<u8>>>,
+    ram: Ram,
 }
 
 impl Cpu {
-    pub fn new(rom: Rc<Vec<u8>>, ram: Rc<RefCell<Vec<u8>>>) -> Self {
+    pub fn new(rom: Rc<Vec<u8>>, ram: Ram) -> Self {
         Cpu {
             registers: Registers::default(),
             rom,
@@ -23,6 +24,23 @@ impl Cpu {
     pub fn reset(&mut self) {
         self.registers = Registers::default();
         self.registers.program_counter = self.read_word(0xfffc)
+    }
+
+    pub fn run(&mut self) {
+        let opcode = self.fetch();
+        let instruction = Instruction::from_opcode(opcode);
+        match instruction.kind {
+            Kind::SEI => {
+                self.registers.status.irq_prohibited = true;
+            }
+            _ => {}
+        }
+    }
+
+    fn fetch(&mut self) -> u8 {
+        let value = self.read(self.registers.program_counter);
+        self.registers.program_counter += 1;
+        value
     }
 
     fn read(&self, addr: u16) -> u8 {
@@ -50,9 +68,11 @@ impl Cpu {
     }
 }
 
+type Ram = Rc<RefCell<Vec<u8>>>;
+
 #[cfg(test)]
 mod test {
-    use super::Cpu;
+    use super::{Cpu, Ram};
     use std::{cell::RefCell, rc::Rc};
 
     #[test]
@@ -68,5 +88,28 @@ mod test {
 
         cpu.reset();
         assert_eq!(cpu.get_registers().program_counter, 0x8000);
+    }
+
+    #[test]
+    fn test_instruction_sei() {
+        let (mut cpu, mut _ram) = prepare(&[0x78]);
+        cpu.run();
+        assert!(cpu.get_registers().status.irq_prohibited);
+    }
+
+    fn prepare(initial_bytes: &[u8]) -> (Cpu, Ram) {
+        let mut rom = vec![0; 0x8000];
+        rom[0x7ffc] = 0x00;
+        rom[0x7ffd] = 0x80;
+
+        for (i, b) in initial_bytes.iter().enumerate() {
+            rom[i] = b.clone();
+        }
+
+        let rom = Rc::new(rom);
+        let ram = Rc::new(RefCell::new(vec![0; 0x800]));
+        let mut cpu = Cpu::new(rom, ram.clone());
+        cpu.reset();
+        (cpu, ram)
     }
 }
